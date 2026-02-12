c61d7180234c01c6a30c42076e436571e0b619a0
GoychakBadalova <gbadalova20360@gmail.com>
2026-02-12 11:09:43 +0400

    Add files via upload

---
+++ Invoice.java
@@ -0,0 +1,62 @@
+package week04.invoice;
+
+public class Invoice {
+    private String partNumber;
+    private String partDescription;
+    private int quantity;
+    private double pricePerItem;
+
+    public Invoice(String partNumber,String partDescription,int quantity,double pricePerItem){
+        this.partNumber=partNumber;
+        this.partDescription=partDescription;
+        if(quantity<0){
+            this.quantity=0;
+        } else{
+            this.quantity=quantity;
+        }
+        if(pricePerItem<0){
+            this.pricePerItem=0.0;
+        } else{
+            this.pricePerItem=pricePerItem;
+        }
+    }
+
+    public String getPartDescription(){
+        return partDescription;
+    }
+
+    public String getPartNumber(){
+        return partNumber;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public double getPricePerItem(){
+        return pricePerItem;
+    }
+
+    public void setPartNumber(String partNumber){
+        this.partNumber=partNumber;
+    }
+    public void setPartDescription(String partDescription){
+        this.partDescription=partDescription;
+    }
+
+    public void setQuantity(int quantity){
+        if(quantity<0){
+            this.quantity=0;
+        }
+        else{
+            this.quantity=quantity;
+        }
+    }
+    public void setPricePerItem(double pricePerItem){
+        if(pricePerItem<0){
+            this.pricePerItem=0.0;
+        }
+        else{
+            this.pricePerItem=pricePerItem;
+        }
+    }
+
+}
